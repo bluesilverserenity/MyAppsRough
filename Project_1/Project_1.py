756ebb793609a185dbd25b246d65fffb05bd953a
bluesilverserenity <idamunyad@gmail.com>
2026-05-13 18:51:16 +0100

    second working draft

--- Project_1/Project_1.py
+++ Project_1/Project_1.py
@@ -1,10 +1,30 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 def translate(word):
-    return data[word]
+    word = word.lower()
+    if word in data:
+        return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        ans = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word, data.keys())[0])
+        if ans == "Y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif ans == "N":
+            return "Word does not exist. Please double check it."
+        else:            return "We didn't understand your entry."
+    else:
+        return "Word not found."
 
-word = input("Enter a word: ")
+word = ""
 
-print(translate(word))
+while word != "close":
+    word = input("Enter a word: ")
+    output = translate(word)
+    if type(output) == list:
+        for item in output:
+            print(item)
+    else:
+        print(output)
+        continue
