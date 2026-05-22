0b026df47d88ae455dbd58b56d3ab789a85347da
bluesilverserenity <idamunyad@gmail.com>
2026-05-22 19:03:10 +0100

    added markers on map

--- Project_2/rough.py
+++ Project_2/rough.py
@@ -3,7 +3,8 @@ map = fol.Map(location=[37.55043934921041, 126.99701154232895], zoom_start=12, T
 
 fg = fol.FeatureGroup(name="My Map")
 
-fg.add_child(fol.Marker(location=[37.55043934921041, 126.99701154232895], popup="This is SEOUL", icon=fol.Icon(color="blue")))
+for coordinates in [[37.5, 126.9], [37.6, 126.8]]:
+    fg.add_child(fol.Marker(location=coordinates, popup="This is SEOUL", icon=fol.Icon(color="blue")))
 
 map.add_child(fg)
-map.save("map.html")
+map.save("map1.html")
