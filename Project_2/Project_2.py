5fec02884c3f7d5121558a2ae4b92ed07975b92d
bluesilverserenity <idamunyad@gmail.com>
2026-06-04 10:30:42 +0100

    naming my markers

--- Project_2/Project_2.py
+++ Project_2/Project_2.py
@@ -10,7 +10,7 @@ map = fol.Map(location=[48.776798, -121.810997], zoom_start=6, tiles="OpenStreet
 
 fg = fol.FeatureGroup(name="Volcanoes Map")
 for i, j, k in zip(lat, lon, elev):
-    fg.add_child(fol.Marker(location=[i, j], popup=k, icon=fol.Icon(color="red")))
+    fg.add_child(fol.Marker(location=[i, j], popup=str(k)+" m", icon=fol.Icon(color="red")))
 
 map.add_child(fg)
 map.save("volmap.html")
