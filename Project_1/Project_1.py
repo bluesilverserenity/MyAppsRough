8a1a24fe7e6b8879e534e50c6afa2253a8f9d266
bluesilverserenity <idamunyad@gmail.com>
2026-06-04 10:00:30 +0100

    added comments

--- Project_1/Project_1.py
+++ Project_1/Project_1.py
@@ -1,16 +1,21 @@
 import json
 from difflib import get_close_matches
 
+# load JSON data from `data.json` into a Python dictionary named `data`
 data = json.load(open("data.json"))
 
 def translate(word):
+    # normalize input to lowercase for case-insensitive lookup
     word = word.lower()
+    # if the exact word exists in data, return its definition(s)
     if word in data:
         return data[word]
+    # if no exact match but there are close matches, ask user to confirm the suggestion
     elif len(get_close_matches(word, data.keys())) > 0:
-        ans = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word, data.keys())[0])
+        ans = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word, data.keys())[0]) # get the closest match and ask user to confirm
         if ans == "Y":
             return data[get_close_matches(word, data.keys())[0]]
+        # if user denies, tell them the word does not exist
         elif ans == "N":
             return "Word does not exist. Please double check it."
         else:            return "We didn't understand your entry."
@@ -19,9 +24,11 @@ def translate(word):
 
 word = ""
 
+# keep asking the user for words until they type "close"
 while word != "close":
     word = input("Enter a word: ")
     output = translate(word)
+    # if the output is a list of definitions, print each on a new line
     if type(output) == list:
         for item in output:
             print(item)
