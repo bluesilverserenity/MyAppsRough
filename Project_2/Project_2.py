f234effbdaa175027b5308c14c39ea1142641b50
bluesilverserenity <idamunyad@gmail.com>
2026-06-17 12:12:39 +0100

    adding layers

--- Project_2/Project_2.py
+++ Project_2/Project_2.py
@@ -20,5 +20,7 @@ fg = fol.FeatureGroup(name="Volcanoes Map")
 for i, j, k in zip(lat, lon, elev):
     fg.add_child(fol.Marker(location=[i, j], popup=str(k)+" m", icon=fol.Icon(color=color_marker(k))))
 
+fg.add_child(fol.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(), style_function=lambda x: {"fillColor": "green" if x["properties"]["POP2005"] < 10000000 else "orange" if 10000000 <= x["properties"]["POP2005"] < 20000000 else "red"}))
+
 map.add_child(fg)
 map.save("volmap.html")
